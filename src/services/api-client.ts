5fc192aaf0c5fede7a2ef59ec613c9a22fdee13b
Decency Okobia <learncodingdee@yahoo.com>
2025-04-13 22:33:12 +0100

    removed env file to fix loading bug

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios, {CanceledError} from "axios";
-export const apiKey = import.meta.env.VITE_SPOONOCULAR_KEY;
+export const apiKey = '33b6917cedfa47f6ba02a96d27cd3548'
 
 export default axios.create({
     baseURL: `https://api.spoonacular.com/recipes/`
